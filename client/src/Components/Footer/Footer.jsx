31b08ed90a542d60e7425a98d4834db7fda1f0fc
Ayush <unitedestates.ayush10@gmail.com>
2023-06-14 01:31:41 +0530

    landing page complete

---
+++ client/src/Components/Footer/Footer.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Logo from "../../Assets/Logo.svg";
+import { BsTwitter } from "react-icons/bs";
+import { SiLinkedin } from "react-icons/si";
+import { BsYoutube } from "react-icons/bs";
+import { FaFacebookF } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className="footer-wrapper">
+      <div className="footer-section-one">
+        <div className="footer-logo-container">
+          <img src={Logo} alt="" />
+        </div>
+        <div className="footer-icons">
+          <BsTwitter />
+          <SiLinkedin />
+          <BsYoutube />
+          <FaFacebookF />
+        </div>
+      </div>
+      <div className="footer-section-two">
+        <div className="footer-section-columns">
+          <span>hacking</span>
+          <span>Help</span>
+          <span>Share</span>
+          <span>code</span>
+          <span>more info</span>
+          <span>Work</span>
+        </div>
+        <div className="footer-section-columns">
+          <span>244-5333-7783</span>
+          <span>hello@hack.com</span>
+          <span>press@hack.com</span>
+          <span>contact@hack.com</span>
+        </div>
+        <div className="footer-section-columns">
+          <span>Terms & Conditions</span>
+          <span>Privacy Policy</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- client/src/Components/home/home.jsx
+++ client/src/Components/home/home.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+import BannerBackground from "../../Assets/home-banner-background.png";
+import BannerImage from "../../Assets/prog.svg";
+import { FiArrowRight } from "react-icons/fi";
 
 const home = () => {
   return (
-    <div>home</div>
-  )
-}
+    <div className="home-container">
+      <Navbar />
+      <div className="home-banner-container">
+        <div className="home-bannerImage-container">
+          <img src={BannerBackground} alt="" />
+        </div>
+        <div className="home-text-section">
+          <h1 className="primary-heading">
+            Lorem ipsum consectetur adipisicing elit.
+          </h1>
+          <p className="primary-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Exercitationem quibusdam perferendis praesentium consequatur ullam!
+          </p>
+          <button className="secondary-button">
+            button <FiArrowRight />
+          </button>
+        </div>
+        <div className="home-image-container">
+          <img src={BannerImage} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default home
+export default home;
